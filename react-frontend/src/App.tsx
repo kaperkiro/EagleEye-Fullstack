b785a57a0fc61d19e00119db019277a13d453801
Daniel Alchasov <danal315@student.liu.se>
2025-04-01 18:11:47 +0200

    removing unneeded index.css and created App.css moved all inline css from App.tsx to App.css

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -3,109 +3,46 @@ import Logo from "./assets/logo.png";
 import LiveViewData from "./components/LiveView";
 import HeatMapData from "./components/HeatMap";
 import LarmData from "./components/Alarms";
+import "./css/App.css";
 
 function App() {
-  // State to track the active button index
   const [activeIndex, setActiveIndex] = useState(0);
 
-  // Update active index on button click
-  const handleButtonClick = (index: React.SetStateAction<number>) => {
+  const handleButtonClick = (index: number) => {
     console.log("Button clicked!", index);
     setActiveIndex(index);
   };
 
   let SiteData;
-  if (activeIndex == 0) {
+  if (activeIndex === 0) {
     SiteData = <LiveViewData />;
-  } else if (activeIndex == 1) {
+  } else if (activeIndex === 1) {
     SiteData = <HeatMapData />;
-  } else if (activeIndex == 2) {
+  } else if (activeIndex === 2) {
     SiteData = <LarmData />;
   }
 
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        overflow: "hidden",
-        flexDirection: "column",
-        margin: 0, // reset default margin if needed
-      }}
-    >
+    <div className="app-container">
       {/* Header Section */}
-      <header
-        style={{
-          borderBottom: "solid",
-          display: "flex",
-          backgroundColor: "#FFFFFF",
-          padding: "20px",
-          textAlign: "center",
-        }}
-      >
-        <img
-          src={Logo}
-          alt="logga"
-          style={{
-            width: "100px",
-            height: "100px",
-          }}
-        />
-        <h1 style={{ margin: 20 }}>EagleEye</h1>
-
-        <div
-          className="ButtonContainer"
-          style={{
-            display: "flex",
-            backgroundColor: "#ffffff",
-            textAlign: "center",
-            alignItems: "center",
-            flexDirection: "row",
-            marginLeft: "120px",
-          }}
-        >
+      <header className="app-header">
+        <img src={Logo} alt="logga" className="app-logo" />
+        <h1>EagleEye</h1>
+        <div className="button-container">
           <button
-            style={{
-              border: "solid black",
-              marginLeft: 40,
-              backgroundColor: activeIndex === 0 ? "#3B82F6" : "white",
-              width: "150px",
-              height: "50px",
-              fontSize: "24px",
-              borderRadius: "20px",
-              color: activeIndex === 0 ? "white" : "black",
-            }}
+            className={`toggle-button ${activeIndex === 0 ? "active" : ""}`}
             onClick={() => handleButtonClick(0)}
           >
             Live View
           </button>
           <button
-            style={{
-              border: "solid black",
-              marginLeft: 50,
-              backgroundColor: activeIndex === 1 ? "#3B82F6" : "white",
-              width: "150px",
-              height: "50px",
-              fontSize: "24px",
-              borderRadius: "20px",
-              color: activeIndex === 1 ? "white" : "black",
-            }}
+            className={`toggle-button ${activeIndex === 1 ? "active" : ""}`}
             onClick={() => handleButtonClick(1)}
           >
             Historical
           </button>
           <button
-            style={{
-              border: "solid black",
-              marginLeft: 50,
-              backgroundColor: activeIndex === 2 ? "#3B82F6" : "white",
-              width: "150px",
-              height: "50px",
-              fontSize: "24px",
-              borderRadius: "20px",
-              color: activeIndex === 2 ? "white" : "black",
-            }}
+            className={`toggle-button ${activeIndex === 2 ? "active" : ""}`}
             onClick={() => handleButtonClick(2)}
           >
             Larms
